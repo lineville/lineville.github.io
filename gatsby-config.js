20e52b292ff92fb6152367014423c28f25a0c68b
Liam Neville <lineville@github.com>
2023-08-17 16:26:42 -0400

    tweak

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Liam Neville',
     description:
       'Liam Neville is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://liamneville.com', // No trailing slash allowed!
+    siteUrl: 'https://lineville.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
